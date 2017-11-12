be386816e73ef9c293ce19a694296b6547f385b7
dawnkd <dawnkd@rpi.edu>
2017-11-12 06:26:45 -0500

    added options menu with login option and fixed fab in view question

--- app/src/main/java/com/crowdsourced_burrito/burrito/LoginActivity.java
+++ app/src/main/java/com/crowdsourced_burrito/burrito/LoginActivity.java
@@ -7,7 +7,7 @@ import android.support.v7.app.AppCompatActivity;
  * Created by dawnkd on 11/12/2017.
  */
 
-public class login extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
