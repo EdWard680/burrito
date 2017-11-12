82fbb5f4da4b549254b77bc222675c5e362e2bb2
colingo1 <colin.goldberg@asdnh.org>
2017-11-12 09:45:44 -0500

    Answer Adapter added

--- app/src/main/java/com/crowdsourced_burrito/burrito/ViewQuestionActivity.java
+++ app/src/main/java/com/crowdsourced_burrito/burrito/ViewQuestionActivity.java
@@ -1,16 +1,40 @@
 package com.crowdsourced_burrito.burrito;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 /**
  * Created by dawnkd on 11/12/2017.
  */
 
 public class ViewQuestionActivity extends AppCompatActivity {
+
+    class AnswersAdapter extends ArrayAdapter<Answer>
+    {
+        public AnswersAdapter(Context context, ArrayList<Answer> answers)
+        {
+            super(context,R.layout.answer_list_item,answers);
+        }
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent)
+        {
+            Answer answer = getItem(position);
+            TextView textView = (TextView)convertView.findViewById(R.id.answer_textView);
+            textView.setText(answer.getContent());
+            ((TextView)(convertView.findViewById(R.id.answerScore).findViewById(R.id.score))).setText(answer.getRating());
+            return convertView;
+
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
