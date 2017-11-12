5173f6b74dbf407deeb29bf54c8e7eed30778585
dawnkd <dawnkd@rpi.edu>
2017-11-12 04:56:00 -0500

    updated manifest

---
+++ app/src/main/java/com/crowdsourced_burrito/burrito/ViewQuestionActivity.java
@@ -0,0 +1,20 @@
+package com.crowdsourced_burrito.burrito;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by dawnkd on 11/12/2017.
+ */
+
+public class ViewQuestionActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.view_question_activity);
+
+    }
+
+
+
+}
